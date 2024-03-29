a1abb730a5e3b986b72c131ffd23de7100c5c7c8
Max Lalleman <max.lalleman@ldas-pcdev8.ligo.caltech.edu>
2024-03-29 08:13:11 -0700

    Adding cdf to hdf + Adding first couple of notebooks for plots in paper.

---
+++ data/download_data_from_zenodo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Download and unzip
+# curl https://sandbox.zenodo.org/record/1246333/files/bbh_time_delay_data.zip --output "bbh_time_delay_data.zip"
+# unzip bbh_time_delay_data.zip
+
+# Move input data to ../input/
+
+# Move input data to ../
+
+# Remove original zip files and directory
